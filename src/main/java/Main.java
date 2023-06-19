ec201c7865cda6f8a8d0841dd5d1a1c4e85083e2
Ilya Sayenko <Ilya.Sayenko@nexign.com>
2023-06-19 17:40:33 +0300

    feature: обработка исключений в main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,24 +12,34 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get(getArgumentValue("--input", args));
-        Path outPath = Paths.get(getArgumentValue("--output", args));
+        try {
+            Path path = Paths.get(getArgumentValue("--input", args));
+            Path outPath = Paths.get(getArgumentValue("--output", args));
 
-        DataReader dataReader = new DataReader();
-        DataWriter dataWriter = new DataWriter();
-        MoneySplitter moneySplitter = new MoneySplitter();
+            DataReader dataReader = new DataReader();
+            DataWriter dataWriter = new DataWriter();
+            MoneySplitter moneySplitter = new MoneySplitter();
 
-        FileReader fileReader = new FileReader(path.toFile(), StandardCharsets.UTF_8);
+            FileReader fileReader = new FileReader(path.toFile(), StandardCharsets.UTF_8);
 
-        InputData inputData = dataReader.readData(fileReader);
-        OutputData outputData = moneySplitter.split(inputData);
+            InputData inputData = dataReader.readData(fileReader);
+            OutputData outputData = moneySplitter.split(inputData);
 
-        dataWriter.writeData(outputData, new OutputStreamWriter(System.out));
-        dataWriter.writeData(outputData, new FileWriter(outPath.toFile(), StandardCharsets.UTF_8));
+            dataWriter.writeData(outputData, new OutputStreamWriter(System.out));
+            dataWriter.writeData(outputData, new FileWriter(outPath.toFile(), StandardCharsets.UTF_8));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Invalid arguments, expected --input and --output");
+        }
     }
 
     private static String getArgumentValue(String argName, String[] args) {
         int argIndex = Arrays.asList(args).indexOf(argName);
+
+        if (argIndex == -1) {
+            throw new IllegalArgumentException();
+        }
 
         return args[argIndex + 1];
     }
