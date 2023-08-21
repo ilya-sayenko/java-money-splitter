8091bc495d72986a80693daba19dfed83b164319
ilya-sayenko <ilyasinus@yandex.ru>
2023-08-21 13:30:46 +0300

    moved main

--- money-splitter-rest/src/main/java/org/example/moneysplitter/MoneySplitterRestApplication.java
+++ money-splitter-rest/src/main/java/org/example/moneysplitter/MoneySplitterRestApplication.java
@@ -1,4 +1,4 @@
-package org.example.moneysplitter.party;
+package org.example.moneysplitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
